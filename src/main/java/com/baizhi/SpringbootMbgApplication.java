7b780b99dfeb5dec75e2020f726fa12f70c24648
chen <1773420864@qq.com>
2018-12-16 10:04:45 +0800

    springboot-mbg

---
+++ src/main/java/com/baizhi/SpringbootMbgApplication.java
@@ -0,0 +1,14 @@
+package com.baizhi;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.baizhi.mapper")
+public class SpringbootMbgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMbgApplication.class, args);
+    }
+}
---
+++ src/test/java/com/baizhi/SpringbootMbgApplicationTests.java
@@ -0,0 +1,23 @@
+package com.baizhi;
+
+import com.baizhi.entity.Emp;
+import com.baizhi.mapper.EmpMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootMbgApplicationTests {
+    @Autowired
+    EmpMapper mapper;
+    @Test
+    public void contextLoads() {
+        Emp emp = mapper.selectByPrimaryKey(1);
+        System.out.println(emp);
+    }
+
+}
